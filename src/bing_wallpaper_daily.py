ae9fbf5f2e7a896183ef3164075d4a21bffbacd3
Line <mapleblue2007@gmail.com>
2022-08-08 11:27:53 +0800

    update user-agent

--- src/bing_wallpaper_daily.py
+++ src/bing_wallpaper_daily.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     folder_path = './images'
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     proxies = {}
 
